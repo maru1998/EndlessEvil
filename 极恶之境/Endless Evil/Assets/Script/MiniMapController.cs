a4a58b707531f86250c6450bb4305ed5f844c017
user.email <lik13@miamioh.edu>
2020-10-04 07:37:11 +0800

    Mini Map

---
+++ 极恶之境/Endless Evil/Assets/Script/MiniMapController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniMapController : MonoBehaviour
+{
+    public GameObject Panel;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("m"))
+        {
+            Panel.SetActive(!Panel.activeSelf);
+        }
+    }
+}
